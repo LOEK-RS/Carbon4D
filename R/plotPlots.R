6e6ce2778d8654c75b65a908745920edc0fb19e5
MaikenBaumberger <maiken.baumberger@uni-muenster.de>
2022-01-20 11:10:48 +0100

    some initial commits

---
+++ R/plotPlots.R
@@ -0,0 +1,20 @@
+#' Plot the Carbon4D plots
+#' @description
+#' @param url URL of the Carbon-4D data base
+#' @details 
+#' @return A mapview output
+#' @author
+#' Hanna Meyer
+#' @examples
+#' \dontrun{
+#' plotPlots()
+#' }
+#' @export plotPlots
+#' @aliases plotPlots
+plotPlots <- function(url){
+  plots <- readr::read_csv(gsheet::construct_download_url(url))
+  plots <- plots[plots$PlotID!="P0000",]
+  plots_sf <- sf::st_as_sf(plots,coords = c("Lon","Lat"))
+  sf::st_crs(plots_sf) <- 4326
+  mapview::mapview(plots_sf)
+}
---
+++ R/test.R
@@ -0,0 +1,17 @@
+#' this is a test function
+#' @description
+#' @param x whatever
+#' @details 
+#' @return just a print
+#' @author
+#' Hanna Meyer
+#' @examples
+#' \dontrun{
+#' test("hallo")
+#' }
+#' @export test
+#' @aliases test
+
+test <- function(x){
+  print(x)
+}
