5a1fd60dd70368f5e9adde379c1da167f1e7c2c8
MaikenBaumberger <maiken.baumberger@uni-muenster.de>
2022-06-20 12:40:46 +0200

    modify load probe data

--- R/load_monthly_probe_data.R
+++ R/load_monthly_probe_data.R
@@ -16,7 +16,9 @@
 load_monthly_probe_data <- function(path){
   csv_files_monthly_probe_data = list.files(paste0(path,"/ProbeDataMonthly"), pattern = ".csv",full.names = T)
   csv_names_monthly_probe_data = stringr::str_remove(list.files(paste0(path,"/ProbeDataMonthly"), pattern = ".csv"), ".csv")
-  csv_list_monthly_probe_data = lapply(csv_files_monthly_probe_data, read.csv) 
+  csv_list_monthly_probe_data = lapply(csv_files_monthly_probe_data,function(i){read.csv(i, header=F,skip = 1)})
+  colnames <- c("datetime","T_org","T_05","T_15","T_25","T_35","T_45","T_55","T_65","T_75","T_85","T_95","T_105","T_115","M_org","M_05","M_15","M_25","M_35","M_45","M_55","M_65","M_75","M_85","M_95","M_105","M_115")
+  csv_list_monthly_probe_data <- lapply(csv_list_monthly_probe_data, setNames, nm = colnames)
   names(csv_list_monthly_probe_data) = csv_names_monthly_probe_data
-  assign("csv_list_Monthly_probe_data", csv_list_monthly_probe_data, envir = .GlobalEnv)
+  assign("csv_list_monthly_probe_data", csv_list_monthly_probe_data, envir = .GlobalEnv)
 }
