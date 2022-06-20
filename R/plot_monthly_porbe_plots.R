3684544483ebeca352dae545f1a6b91b9fe423bb
MaikenBaumberger <maiken.baumberger@uni-muenster.de>
2022-06-20 10:27:13 +0200

    correction monthly probes

--- R/plot_monthly_porbe_plots.R
+++ R/plot_monthly_porbe_plots.R
@@ -5,7 +5,7 @@
 #' @author Maiken Baumberger
 #' @examples plot_monthly_porbe_plots(path="C:/Users/maike/Desktop/Carbon4D/DownloadGitData/ProbeMetaDataMonthly/ProbeMetaDataMonthly.csv")
 #' \dontrun{
-#' plot_Monthly_porbe_plots()
+#' plot_monthly_porbe_plots()
 #' }
 #' @export plot_monthly_porbe_plots
 #' @aliases plot_monthly_porbe_plots
@@ -13,7 +13,7 @@
 
 
 
-plot_Monthly_porbe_plots <- function(path){
+plot_monthly_porbe_plots <- function(path){
   ProbeMetaDataMonthly = read.csv(paste0(path,"/ProbeMetaDataMonthly/ProbeMetaDataMonthly.csv"))
   assign("ProbeMetaDataMonthly", ProbeMetaDataMonthly, envir = .GlobalEnv)
   plots <- ProbeMetaDataMonthly
