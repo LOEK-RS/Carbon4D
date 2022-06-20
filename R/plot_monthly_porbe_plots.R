c91d41773ddcfe9e77778be47e977807415d6d2a
MaikenBaumberger <maiken.baumberger@uni-muenster.de>
2022-06-20 10:24:35 +0200

    plo monthly probes

--- R/plot_monthly_porbe_plots.R
+++ R/plot_monthly_porbe_plots.R
@@ -3,9 +3,9 @@
 #' @param path path to the local saved 
 #' @return A mapview output
 #' @author Maiken Baumberger
-#' @examples plot_monthly_porbe_plots(path="C:/Users/maike/Desktop/Carbon4D/DownloadGitData/ProbeMetaData/ProbeMetaData.csv")
+#' @examples plot_monthly_porbe_plots(path="C:/Users/maike/Desktop/Carbon4D/DownloadGitData/ProbeMetaDataMonthly/ProbeMetaDataMonthly.csv")
 #' \dontrun{
-#' plot_monthly_porbe_plots()
+#' plot_Monthly_porbe_plots()
 #' }
 #' @export plot_monthly_porbe_plots
 #' @aliases plot_monthly_porbe_plots
@@ -13,12 +13,10 @@
 
 
 
-plot_monthly_porbe_plots <- function(path){
-  ProbeMetaData = read.csv(paste0(path,"/ProbeMetaData/ProbeMetaData.csv"))
-  assign("ProbeMetaData", ProbeMetaData, envir = .GlobalEnv)
-  plots <- ProbeMetaData
+plot_Monthly_porbe_plots <- function(path){
+  ProbeMetaDataMonthly = read.csv(paste0(path,"/ProbeMetaDataMonthly/ProbeMetaDataMonthly.csv"))
+  assign("ProbeMetaDataMonthly", ProbeMetaDataMonthly, envir = .GlobalEnv)
+  plots <- ProbeMetaDataMonthly
   plots_sf <- sf::st_as_sf(plots,coords = c("lon","lat"), crs = 4326)
   mapview::mapview(plots_sf)
 }
-
-
