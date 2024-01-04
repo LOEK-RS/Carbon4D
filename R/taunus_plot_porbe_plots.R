271876058af35f18e525bdb5276e7cd14a27cc83
MaikenBaumberger <maiken.baumberger@uni-muenster.de>
2024-01-04 13:51:26 +0100

    plot taunus

---
+++ R/taunus_plot_porbe_plots.R
@@ -0,0 +1,22 @@
+#' mapview plot of the locations of the taunus porbe data
+#' @description mapview plot of the locations of the taunus porbe data
+#' @param path path to the local saved 
+#' @return A mapview output
+#' @author Maiken Baumberger
+#' @examples taunus_plot_porbe_plots()
+#' \dontrun{
+#' taunus_plot_porbe_plots()
+#' }
+#' @export taunus_plot_porbe_plots
+#' @aliases taunus_plot_porbe_plots
+
+
+
+
+taunus_plot_porbe_plots <- function(path){
+  TaunusProbeMetaData = read.csv(paste0(path,"/TaunusProbeMetaData/TaunusProbeMetaData"))
+  assign("TaunusProbeMetaData", TaunusProbeMetaData, envir = .GlobalEnv)
+  plots <- TaunusProbeMetaData
+  plots_sf <- sf::st_as_sf(plots,coords = c("lon","lat"), crs = 4326)
+  mapview::mapview(plots_sf)
+}
